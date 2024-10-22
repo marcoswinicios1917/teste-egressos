dc05f3f1c4a826c72c004f5329d1bbbb8f69eacf
Marcos1917 <marcos@ufdpar.edu.br>
2024-10-22 16:02:19 -0300

    formulário de egressos

--- src/layouts/sections/components/View.vue
+++ src/layouts/sections/components/View.vue
@@ -1,166 +1,287 @@
-<script setup>
-import { ref } from "vue";
-
-// import Prism Editor
-import { PrismEditor } from "vue-prism-editor"; //
-import "prismjs/themes/prism.css"; // import the styles somewhere
-import "vue-prism-editor/dist/prismeditor.min.css";
-
-// import highlighting library (you can use any library you want just return html string)
-import prism from "prismjs";
-
-// clipboard
-import useClipboard from "vue-clipboard3";
-
-// prop
-const props = defineProps({
-  id: {
-    type: String,
-    required: true,
-  },
-  code: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  height: {
-    type: String,
-    default: "",
-  },
-});
-
-// variables
-const { toClipboard } = useClipboard();
-const editorCode = ref(props.code);
-
-// functions
-const copy = async (event) => {
-  try {
-    await toClipboard(editorCode.value);
-    const el = event.target.parentElement;
-    var alert = document.createElement("div");
-    alert.classList.add(
-      "alert",
-      "alert-success",
-      "position-absolute",
-      "top-5",
-      "border-0",
-      "text-white",
-      "w-25",
-      "end-0",
-      "start-0",
-      "mt-2",
-      "mx-auto",
-      "py-2"
-    );
-    alert.style.transform = "translate3d(0px, 0px, 0px)";
-    alert.style.opacity = "0";
-    alert.style.transition = ".35s ease";
-    setTimeout(function () {
-      alert.style.transform = "translate3d(0px, 20px, 0px)";
-      alert.style.setProperty("opacity", "1", "important");
-    }, 100);
-    alert.innerHTML = "Código copiado com sucesso!";
-    el.parentElement.appendChild(alert);
-    setTimeout(function () {
-      alert.style.transform = "translate3d(0px, 0px, 0px)";
-      alert.style.setProperty("opacity", "0", "important");
-    }, 2000);
-    setTimeout(function () {
-      el.parentElement.querySelector(".alert").remove();
-    }, 2500);
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-const highlighter = (code) => {
-  return prism.highlight(code, prism.languages.html);
-};
-</script>
-
 <template>
-  <div
-    class="position-relative border-radius-xl overflow-hidden shadow-lg mb-7"
-  >
-    <div class="container border-bottom">
-      <div class="row justify-space-between py-2">
-        <div class="col-lg-3 me-auto">
-          <p class="lead text-dark pt-1 mb-0">{{ title }}</p>
-        </div>
-        <div class="col-lg-3">
-          <div class="nav-wrapper position-relative end-0">
-            <ul class="nav nav-pills nav-fill flex-row p-1" role="tablist">
-              <li class="nav-item">
-                <a
-                  class="nav-link mb-0 px-0 py-1 active"
-                  data-bs-toggle="tab"
-                  :href="`#preview-${id}`"
-                  role="tab"
-                  aria-selected="true"
-                >
-                  <i class="fas fa-search text-sm me-2"></i> Pesquisar
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link mb-0 px-0 py-1"
-                  data-bs-toggle="tab"
-                  :href="`#code-${id}`"
-                  role="tab"
-                  aria-selected="false"
-                >
-                  <i class="fas fa-edit text-sm me-2"></i> Cadastrar
-                </a>
-              </li>
-            </ul>
+  <div class="container mt-5">
+    <div
+      class="position-relative border-radius-xl overflow-hidden shadow-lg mb-7"
+    >
+      <div class="container border-bottom">
+        <div class="row justify-content-between py-2">
+          <div class="col-lg-3 me-auto">
+            <p class="lead text-dark pt-1 mb-0">{{ title }}</p>
+          </div>
+          <div class="col-lg-3">
+            <div class="nav-wrapper position-relative end-0">
+              <ul
+                class="nav nav-pills nav-fill d-flex justify-content-between p-1"
+                role="tablist"
+              >
+                <li class="nav-item">
+                  <a
+                    class="nav-link mb-0 px-0 py-1 active"
+                    data-bs-toggle="tab"
+                    :href="'#preview-' + id"
+                    role="tab"
+                    aria-selected="true"
+                  >
+                    <i class="fas fa-search text-sm me-2"></i> Pesquisar
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link mb-0 px-0 py-1"
+                    data-bs-toggle="tab"
+                    :href="'#code-' + id"
+                    role="tab"
+                    aria-selected="false"
+                  >
+                    <i class="fas fa-edit text-sm me-2"></i> Cadastrar
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="tab-content tab-space">
-      <div class="tab-pane active p-2" :id="`preview-${id}`">
-        <div class="overflow-auto" :class="`${height && `height-${height}`}`">
-          <slot />
+      <div class="tab-content tab-space">
+        <div class="tab-pane active p-2" :id="'preview-' + id">
+          <div class="overflow-auto" :class="height ? 'height-' + height : ''">
+            <slot />
+          </div>
         </div>
-      </div>
-      <div class="tab-pane" :id="`code-${id}`">
-        <div class="position-relative p-4 pb-2">
-          <a
-            class="btn btn-sm bg-gradient-dark position-absolute end-4 mt-3 z-index-3"
-            @click="copy($event)"
-            href="javascript:;"
-            ><i class="fas fa-copy text-sm me-1"></i> Copiar</a
-          >
-          <figure class="highlight">
-            <PrismEditor
-              class="p-2 bg-gray-100 border-radius-xl height-800 my-editor"
-              v-model="editorCode"
-              :highlight="highlighter"
-              line-numbers
-              readonly
-            ></PrismEditor>
-          </figure>
+        <div class="tab-pane" :id="'code-' + id">
+          <div class="position-relative p-4 pb-2">
+            <!-- Formulário de Caracterização do Egresso -->
+            <form class="mt-4">
+              <h5>CARACTERIZAÇÃO DO EGRESSO</h5>
+              <div class="mb-3">
+                <label for="nome" class="form-label">Nome completo*</label>
+                <input type="text" class="form-control" id="nome" required />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Gênero*</label>
+                <select class="form-select" required>
+                  <option selected disabled>
+                    Escolha uma das seguintes respostas...
+                  </option>
+                  <option>Mulher Cisgênero</option>
+                  <option>Mulher Transexual</option>
+                  <option>Homem Cisgênero</option>
+                  <option>Homem Transexual</option>
+                  <option>Não Binário</option>
+                  <option>Prefiro não responder</option>
+                  <option>Não sei</option>
+                  <option>Outro</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label for="dataNascimento" class="form-label"
+                  >Data de nascimento*</label
+                >
+                <input
+                  type="date"
+                  class="form-control"
+                  id="dataNascimento"
+                  required
+                />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Nacionalidade</label>
+                <select class="form-select" v-model="nacionalidade" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Brasileiro(a)</option>
+                  <option>Estrangeiro</option>
+                </select>
+              </div>
+              <div class="mb-3" v-if="nacionalidade === 'Estrangeiro'">
+                <label for="pais" class="form-label">Qual País?</label>
+                <input type="text" class="form-control" id="pais" />
+              </div>
+              <div class="mb-3">
+                <label class="form-label"
+                  >Autoidentificação em termos de cor ou raça, de acordo com o
+                  IBGE.*</label
+                >
+                <select class="form-select" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Preta</option>
+                  <option>Parda</option>
+                  <option>Amarela</option>
+                  <option>Indígena</option>
+                  <option>Não sei</option>
+                  <option>Prefiro não responder</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Pessoa Com Deficiência?</label>
+                <select class="form-select">
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Prefiro não responder</option>
+                  <option>Não</option>
+                  <option>Sim</option>
+                </select>
+              </div>
+              <!-- Novo campos adicionados -->
+              <div class="mb-3">
+                <label class="form-label">Estado Civil*</label>
+                <select class="form-select" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Solteiro(a)</option>
+                  <option>Casado(a)</option>
+                  <option>Divorciado(a)</option>
+                  <option>União Estável</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Estado Atual de Residência*</label>
+                <select class="form-select" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>AC</option>
+                  <option>AL</option>
+                  <!-- Outras opções de estado -->
+                  <option>SP</option>
+                  <option>SE</option>
+                  <option>TO</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Município de Residência*</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="municipio"
+                  required
+                />
+              </div>
+
+              <h5>FORMAÇÃO PROFISSIONAL DOS EGRESSOS</h5>
+              <div class="mb-3">
+                <label class="form-label"
+                  >Em qual Instituição cursou sua graduação?*</label
+                >
+                <select class="form-select" v-model="instituicao" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>UFDPar</option>
+                  <option>Outra Instituição</option>
+                </select>
+              </div>
+              <div class="mb-3" v-if="instituicao === 'Outra Instituição'">
+                <label for="outraInstituicao" class="form-label"
+                  >Especificar:</label
+                >
+                <input type="text" class="form-control" id="outraInstituicao" />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Qual sua graduação?*</label>
+                <select class="form-select" v-model="graduacao" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Administração</option>
+                  <option>Biologia</option>
+                  <option>Outro</option>
+                </select>
+              </div>
+              <div class="mb-3" v-if="graduacao === 'Outro'">
+                <label for="outraGraduacao" class="form-label"
+                  >Especificar:</label
+                >
+                <input type="text" class="form-control" id="outraGraduacao" />
+              </div>
+              <div class="mb-3">
+                <label for="anoConclusao" class="form-label"
+                  >Em que ano você concluiu sua graduação?*</label
+                >
+                <input
+                  type="number"
+                  class="form-control"
+                  id="anoConclusao"
+                  required
+                />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Você possui pós-graduação?*</label>
+                <select class="form-select" v-model="posGraduacao" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Sim</option>
+                  <option>Não</option>
+                </select>
+              </div>
+              <div class="mb-3" v-if="posGraduacao === 'Sim'">
+                <label for="posGraduacao" class="form-label"
+                  >Qual sua pós-graduação?</label
+                >
+                <input type="text" class="form-control" id="posGraduacao" />
+              </div>
+              <div class="mb-3">
+                <label class="form-label"
+                  >Você está empregado atualmente?*</label
+                >
+                <select class="form-select" v-model="empregado" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Sim</option>
+                  <option>Não</option>
+                </select>
+              </div>
+              <div class="mb-3" v-if="empregado === 'Sim'">
+                <label for="empregoAtual" class="form-label"
+                  >Qual seu emprego atual?</label
+                >
+                <input type="text" class="form-control" id="empregoAtual" />
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Qual sua área de atuação?*</label>
+                <select class="form-select" required>
+                  <option selected disabled>Escolha uma opção...</option>
+                  <option>Administração</option>
+                  <option>Educação</option>
+                  <option>Saúde</option>
+                  <!-- Outras áreas -->
+                </select>
+              </div>
+              <button type="submit" class="btn btn-primary mt-3">Enviar</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-.my-editor {
-  color: black;
-  font-family: Consolas, Fira Mono, Menlo, Courier, monospace;
-  font-size: 1em;
-  line-height: 1.5;
-  padding: 5px;
-  tab-size: 4;
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: "Formulário de Cadastro",
+    },
+    height: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      nacionalidade: null,
+      instituicao: null,
+      graduacao: null,
+      posGraduacao: null,
+      empregado: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.height-400 {
+  height: 400px;
 }
 
-.prism-editor__textarea:focus {
-  outline: none;
+.height-500 {
+  height: 500px;
+}
+
+.height-600 {
+  height: 600px;
 }
 </style>
